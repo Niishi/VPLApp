790b52163a548877b5183ec61add7b39018f5016
Niishi <loveandpeace831@yahoo.co.jp>
2018-06-26 15:44:47 +0900

    save

--- src/sketch.js
+++ src/sketch.js
@@ -1,24 +1,9 @@
 setup = function(){
-  var canvas = createCanvas(800, 450);
+  var canvas = createCanvas(900, 450);
   canvas.parent('sketch');
 }
 
-var x = 10;
-var y = 10;
-
 draw = function(){
   background(255);
-  rect(x, y, 100, 100);
-  if (keyIsDown(LEFT_ARROW)) {
-    x -= 5;
-  }
-  if (keyIsDown(RIGHT_ARROW)) {
-    x += 5;
-  }
-  if (keyIsDown(UP_ARROW)) {
-    y -= 5;
-  }
-  if (keyIsDown(DOWN_ARROW)) {
-    y += 5;
-  }
+  rect(100, 100, 100, 100);
 }
