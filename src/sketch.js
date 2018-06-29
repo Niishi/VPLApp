f1efcef76c5151ce6306cd1b029002a1e5b79762
Niishi <loveandpeace831@yahoo.co.jp>
2018-06-29 15:07:29 +0900

    save

--- src/sketch.js
+++ src/sketch.js
@@ -1,20 +1,19 @@
 setup = function(){
-  const canvas = createCanvas(800, 550);
+  const canvas = createCanvas(900, 550);
   canvas.parent('sketch');
 }
 
+var x = 0;
+
 draw = function(){
-  background('#ffffff');
+  background(255);
   noStroke();
-  const n = 5;
-  const gradation = 255 / n;
-  const d = 30;
-  const f = 30;
-  const g = d + f;
-  for (var i = 0; i < n; i++ ) {
-    for (var j = 0; j < n; j++ ) {
-      fill(255, i * gradation, j * gradation);
-      rect(30 + i * g, 30 + j * g, d, d);
-    }
+  fill('#99ff99');
+  rect(x, 100, x, x);
+  x = x + 5;
+  if (x > width) {
+    x = 0;
   }
 }
+
+x;
