6f83ba8b4b10dd64e84e0f662b55cd714b2b6aca
Niishi <loveandpeace831@yahoo.co.jp>
2018-10-18 15:29:32 +0900

    upload

--- src/sketch.js
+++ src/sketch.js
@@ -4,3 +4,11 @@ setup = function(){
   var canvas = createCanvas(700, 300);
   canvas.parent('sketch');
 };
+
+draw = function(){
+  var x = 1 + 2 * 3;
+  if (x > 10) {
+    rect(199, 200, 200, 300);
+  }
+  this.rect(,,,);
+};
