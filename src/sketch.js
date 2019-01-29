aeafe6df0356c6a99d7451e713edab5ab279594f
Niishi <loveandpeace831@yahoo.co.jp>
2019-01-30 00:45:45 +0900

    Update sketch.js

--- src/sketch.js
+++ src/sketch.js
@@ -1,2 +1,21 @@
-x      =  1;
-    y  =  2;
+var x = -100;
+
+setup = function(){
+  var canvas = createCanvas(800, 600);
+  canvas.parent(sketch);
+};
+
+draw = function(){
+  background(100);
+  rect(100, 100, 300, 200);
+  if (x < 0) {
+
+
+
+
+
+
+                            x = x * -1 ;}
+  textSize(50);
+  text(0 + x, 200, 200);
+};
